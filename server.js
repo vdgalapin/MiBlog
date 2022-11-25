e2b4508d1ba4a5fe874aa045c1e6473153c576b3
Vyron Galapin <59787986+vdgalapin@users.noreply.github.com>
2022-11-25 15:32:48 -0600

    Initial
    Everything I know.

---
+++ server.js
@@ -0,0 +1,25 @@
+// Loading the modules
+var express = require('express');
+
+// Add the EJS Nodule Module to the server
+const ejs = require('ejs');
+
+// Start the express
+var app = express();
+
+// Set the veiw engine to EJS
+app.set('view engine', 'ejs');
+
+// Render the static files
+app.use(express.static('public'));
+
+// Port
+app.listen(8080);
+
+// Posting the index home page
+app.get('/', function(req, res) {
+
+    // rendering the index page
+    res.render('pages/index')
+
+});
